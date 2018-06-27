b2ffc20cf7fdf5d3ffc33a506ae734d1a23c43ec
Henry Crawford <crawfam@gmail.com>
2018-06-27 15:24:21 -0400

    This is the initial commit of the site.

---
+++ BinaryPlanet/Controllers/ShowPoemsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BinaryPlanet.Controllers
+{
+    [Authorize]
+    public class ShowPoemsController : Controller
+    {
+
+        // see: http://www.dotnet-stuff.com/tutorials/aspnet-mvc/how-to-render-different-layout-in-asp-net-mvc
+
+        public ActionResult Poem(string fileName)
+        {
+            ViewBag.PoemFileName = fileName + ".cshtml";
+            return View("Poem", "_LayoutWhite");
+        }
+    }
+}
