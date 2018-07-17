56cb95a87c41f6391f03369e6eb29e496ef040f6
Henry Crawford <crawfam@gmail.com>
2018-07-17 16:11:04 -0400

    Added poem to section 0010 - And now a word.....

--- BinaryPlanet/Models/ApplicationDbContext.cs
+++ BinaryPlanet/Models/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace BinaryPlanet.Models
         // Add Model Classes Here for Migration into the Database
 
         // Don't forget to update the Database if the migration looks good
+        // How to set up a foreign key: http://www.entityframeworktutorial.net/code-first/foreignkey-dataannotations-attribute-in-code-first.aspx
 
 
         public DbSet<BPUser> BPUsers { get; set; }
